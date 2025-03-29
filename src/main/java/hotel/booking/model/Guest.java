edea0712b469cbac46b9562457c8afb202803d8f
BoomTrading <orytami@gmail.com>
2025-03-29 10:32:17 +0100

    Update Guest e Room models

--- src/main/java/hotel/booking/model/Guest.java
+++ src/main/java/hotel/booking/model/Guest.java
@@ -2,132 +2,88 @@ package hotel.booking.model;
 
 import jakarta.persistence.*;
 import java.time.LocalDate;
-
 import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @Table(name = "Guests")
 public class Guest {
-	
-	@Id
-	@GeneratedValue(strategy =  GenerationType.IDENTITY)
-	private int id;
-	
-	@Column(name = "firstName")
-	private String firstName;
-	
-	@Column(name = "lastName")
-	private String lastName;
-	
-	@Column(name = "email")
-	private String email;
-
-	@Column(name = "phone")
-	private String phone;
-
-	@Column(name = "nationality")
-	private String nationality;
-
-	@Column(name = "country")
-	private String country;
-
-	@Column(name = "city")
-	private String city;
-
-	@Column(name = "state")
-	private String state;
-
-	@Column(name = "province")
-	private String province;
-
-	@Column(name = "address")
-	private String address;
-
-	@Column(name = "birthDay")
-	@DateTimeFormat(pattern = "yyyy-MM-dd")
-	private LocalDate birthDay;
-
-	@Column(name = "identityCard")
-	private String identityCard;
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-	
-	public String getPhone() {
-		return phone;
-	}
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-	public String getNationality() {
-		return nationality;
-	}
-	public void setNationality(String nationality) {
-		this.nationality = nationality;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getProvince() {
-		return province;
-	}
-	public void setProvince(String province) {
-		this.province = province;
-	}
-	public String getAddress() {
-		return address;
-	}
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	public LocalDate getBirthDay() {
-		return birthDay;
-	}
-	public void setBirthDay(LocalDate birthDay) {
-		this.birthDay = birthDay;
-	}
-	public String getIdentityCard() {
-		return identityCard;
-	}
-	public void setIdentityCard(String identityCard) {
-		this.identityCard = identityCard;
-	}
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name = "firstName")
+    private String firstName;
+    
+    @Column(name = "lastName")
+    private String lastName;
+    
+    @Column(name = "email")
+    private String email;
+    
+    @Column(name = "password")
+    private String password; // Campo aggiunto per l'autenticazione
+    
+    @Column(name = "phone")
+    private String phone;
+    
+    @Column(name = "nationality")
+    private String nationality;
+    
+    @Column(name = "country")
+    private String country;
+    
+    @Column(name = "city")
+    private String city;
+    
+    @Column(name = "state")
+    private String state;
+    
+    @Column(name = "province")
+    private String province;
+    
+    @Column(name = "address")
+    private String address;
+    
+    @Column(name = "birthDay")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate birthDay;
+    
+    @Column(name = "identityCard")
+    private String identityCard;
+    
+    @Column(name = "role")
+    private String role; // Campo aggiunto per i ruoli (es. "USER", "ADMIN")
+    
+    // Getter e Setter
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public String getNationality() { return nationality; }
+    public void setNationality(String nationality) { this.nationality = nationality; }
+    public String getCountry() { return country; }
+    public void setCountry(String country) { this.country = country; }
+    public String getCity() { return city; }
+    public void setCity(String city) { this.city = city; }
+    public String getState() { return state; }
+    public void setState(String state) { this.state = state; }
+    public String getProvince() { return province; }
+    public void setProvince(String province) { this.province = province; }
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+    public LocalDate getBirthDay() { return birthDay; }
+    public void setBirthDay(LocalDate birthDay) { this.birthDay = birthDay; }
+    public String getIdentityCard() { return identityCard; }
+    public void setIdentityCard(String identityCard) { this.identityCard = identityCard; }
+    public String getRole() { return role; }
+    public void setRole(String role) { this.role = role; }
 }
-
